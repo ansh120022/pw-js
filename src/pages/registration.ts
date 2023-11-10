c05c19b21f588c8f7cc4be78ce88eab8ea4d48b5
Anastasia <ansh120022@gmail.com>
2023-11-10 20:45:47 +0400

    add registration test

--- src/pages/registration.ts
+++ src/pages/registration.ts
@@ -1,21 +1,45 @@
 import { Page } from 'playwright';
 
-export class RegistrationPage {
+export class AccountPage {
     constructor(page: Page) {
         this.page = page;
         this.returningUserButton = this.page.getByText('Returning user');
+        this.newUserButton = this.page.getByText('New user')
         this.emailInput = this.page.locator('INPUT[type="email"]');
+        this.passwordInput = this.page.locator('INPUT[type="password"]');
+        this.numberInput = this.page.locator('INPUT[type="number"]');
+        this.textInput= this.page.locator('INPUT[type="text"]');
         this.submitButton = this.page.locator('button[type="submit"]');
+
+        this.agreeButton = this.page.getByText('I GET IT, CONTINUE');
     }
 
-    async navigate() {
+    async submitForm(){
+        await this.submitButton.click();
+    }
+
+    async navigateStartPage() {
         await this.page.goto("https://demo.ft-crm.com/");
     }
 
-    async login() {
+    async signIn() {
         await this.returningUserButton.click();
         await this.emailInput.fill('ansh00000@gmail.com');
-        await this.submitButton.click();
-        console.log("")
+        await this.submitForm();
     }
+
+    async signUp(){
+        await this.newUserButton.click();
+        await this.agreeButton.click();
+        await this.emailInput.fill('ansh00000@gmail.com');
+        await this.submitForm();
+        await this.textInput.fill('+357')
+        await this.numberInput.fill('9690000')
+        await this.submitForm();
+        await this.textInput.fill('Anastasiia Sh')
+        await this.submitForm();
+        await this.passwordInput.fill('MYsPASS_999!')
+        await this.submitForm();
+    }
+
 }
--- src/tests/registration.test.ts
+++ src/tests/registration.test.ts
@@ -1,11 +1,20 @@
 import { test, expect } from '@playwright/test';
-import { RegistrationPage } from '../pages/registration';
+import { AccountPage } from '../pages/registration';
 
-test('Login test', async ({ page }) => {
-    const registration = new RegistrationPage(page);
 
-    await registration.navigate();
-    await registration.login();
+test('Registration test', async ({ page }) => {
+    const account = new AccountPage(page);
+
+    await account.navigateStartPage();
+    await account.signUp();
+
+});
+
+test('Authorization test', async ({ page }) => {
+    const account = new AccountPage(page);
+
+    await account.navigateStartPage();
+    await account.signIn();
     const balanceButton = await page.locator('BUTTON[class="button money"]');
     await expect(balanceButton).toBeVisible();
     await page.screenshot({ path: 'login.png' });
