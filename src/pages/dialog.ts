fa5ce0a74f519b02e73ad55a0ac9136ece785d50
Anastasia <ansh120022@gmail.com>
2023-11-30 14:51:50 +0100

    add authorization negative test

---
+++ src/pages/dialog.ts
@@ -0,0 +1,19 @@
+import { Page } from 'playwright';
+
+export class BrowserDialog {
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async acceptExpectedDialog(expectedText: string) {
+        // Accept dialog if text is expected
+        const dialog = await this.page.waitForEvent('dialog');
+        const dialogText = dialog.message();
+        if (dialogText == expectedText) {
+            await dialog.accept();
+        }
+        else {
+            await page.screenshot({ path: 'unexpected_dialog.png' });
+        }
+    }
+}
