79397d027bb0c4aedef2f9cd8d39a61aca5d83ea
Anastasia <ansh120022@gmail.com>
2023-12-01 14:28:49 +0400

    add Top up balance test

--- src/pages/balance.ts
+++ src/pages/balance.ts
@@ -4,9 +4,31 @@ export class BalancePage {
     constructor(page: Page) {
         this.page = page;
         this.balanceButton = this.page.locator('BUTTON[class="button money"]');
+        this.bonus500option = this.page.locator('SELECT[class="form-input"]');
+        this.cardOption = this.page.getByText('Card')
+        this.option500eur = this.page.locator("//button[.='€500']")
+        this.depositApproved = this.page.getByText('Deposit Approved')
+        this.closeModal = this.page.locator("//div[@class='modal__x']")
     }
 
     async getBalanceButtonLocator(){
         return this.balanceButton;
     }
+
+    async getBalanceValue() {
+        const balanceButtonText = await this.page.textContent('BUTTON[class="button money"]')
+        const balanceValue = parseFloat(balanceButtonText.replace('€', ''));
+        return balanceValue;
+    }
+
+    async topUp()
+    {
+        await this.balanceButton.click();
+        await this.bonus500option.selectOption('FastTrackBonus + 500%');
+        await this.cardOption.click();
+        await this.option500eur.click();
+        await this.depositApproved.click();
+        await this.closeModal.click();
+    }
+
 }
